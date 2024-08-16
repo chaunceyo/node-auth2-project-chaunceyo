621fac881ba588172ac0bbff66b6b3ca8cac78c6
Chaunceyo <chaunceyogletree725@gmail.com>
2024-08-16 08:51:48 -0400

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,7 +1,6 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
-
-const restricted = (req, res, next) => {
-  next()
+const User = require('../users/users-model')
+const restricted =  (req, res, next) => {
   /*
     If the user does not provide a token in the Authorization header:
     status 401
@@ -17,7 +16,8 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
-}
+  
+  }
 
 const only = role_name => (req, res, next) => {
   next()
@@ -34,8 +34,7 @@ const only = role_name => (req, res, next) => {
 }
 
 
-const checkUsernameExists = (req, res, next) => {
-  next()
+const checkUsernameExists = async (req, res, next) => {
   /*
     If the username in req.body does NOT exist in the database
     status 401
@@ -43,6 +42,18 @@ const checkUsernameExists = (req, res, next) => {
       "message": "Invalid credentials"
     }
   */
+    try{
+      const [user] = 
+      await User.findBy({username: req.body.username})
+      if(!user){
+        next({status: 401, message: "Invalid credentials"})
+      }else{
+        req.user = user
+        next()
+      }
+    }catch(err){
+      next(err)
+    }
 }
 
 
