ee02a41dc1e0e27648e2a5ec8737f9189da50ba6
Chaunceyo <chaunceyogletree725@gmail.com>
2024-08-16 05:24:51 -0400

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -48,6 +48,16 @@ const checkUsernameExists = (req, res, next) => {
 
 const validateRoleName = (req, res, next) => {
   next()
+  if(!req.body.role_name || !req.body.role_name.trim()){
+    req.role_name = 'student'
+  }else if(req.body.role_name.trim() === 'admin'){
+    next({status: 422, message: "Role name can not be admin"})
+  }else if(req.body.role_name.trim().length > 32){
+    next({status: 422, message: "Role name can not be longer than 32 chars"})
+  }else{
+    req.role_name = req.body.role_name.trim()
+    next()
+  }
   /*
     If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
 
